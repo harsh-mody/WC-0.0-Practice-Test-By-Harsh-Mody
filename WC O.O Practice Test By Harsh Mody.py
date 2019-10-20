e63f1674e84e625593551a68e0dbf0a396733fe2
harshmody30401 <56110469+harshmody30401@users.noreply.github.com>
2019-10-20 21:37:19 +0530

    Add files via upload

---
+++ WC O.O Practice Test By Harsh Mody.py
@@ -0,0 +1,10 @@
+for i in range(1, 11):
+    sum1 = (i*(i+1))/2
+print("Sum Of First 10 Natural No.s : " + str(sum1))
+for i in range(1, 101):
+    sum2 = (i*(i+1))/2
+print("Sum Of First 100 Natural No.s : " + str(sum2))
+for i in range(1, 201):
+    sum3 = (i*(i+1))/2
+print("Sum Of First 200 Natural No.s : " + str(sum3))
+print("Sum Of First 10 Natural No.s + Sum Of First 100 Natural No.s + Sum Of First 200 Natural No.s : " + str(sum1+sum2+sum3))
